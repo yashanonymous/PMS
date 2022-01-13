be6e2f61b05f8abbb1f6413812fccc6fbd88a4a5
yashanonymous <40730688+yashanonymous@users.noreply.github.com>
2022-01-13 19:58:56 +0100

    Add files via upload

--- hibp.py
+++ hibp.py
@@ -1,22 +1,20 @@
 import requests
 import hashlib
 
-def hashing():
-    buff="yash"
+def hashing(buff):
     y=hashlib.sha1(buff.encode('utf-8')).hexdigest()
-    print(y)
-    return y
-y=hashing()
+    print(y.upper())
+    return y.upper()
 o=requests.get ("https://api.pwnedpasswords.com/range/"+y[:5])
-#print(o.text)
-for i in range(len(o.text)):
-    print(o.text)
-    '''if (y==o.text[j:k]):
-        j=j+36
-        k=k+39
-        print("password leaked")
-        break
-    else:
-        j=j+36
-        k=k+39
-        print("not leaked")'''
+print(o.text)
+print("-----------------------------------------------")
+with open("hashes.txt",'w') as hash:
+    hash.write(o.text)
+with open("hashes.txt",'r') as hash:
+    for i in hash:
+        if (y[5:]==i[:35]):
+            print('leaked')
+            print(i)
+            break
+        else:
+            print("not leaked")
